476a1b9c36207095df273364d84a7561fa613578
Valmur Prado <46166097+pradovalmur@users.noreply.github.com>
2025-04-09 11:44:37 +0000

    start dbt models

---
+++ curso/tesouro/models/staging/operacoes.sql
@@ -0,0 +1,33 @@
+{{ config(materialized='table') }}
+
+WITH raw AS (
+    SELECT 
+        "Codigo do Investidor" as codigo_investidor,
+        "Data da Operacao" as data_operacao,
+        "Tipo Titulo" as tipo_titulo,
+        "Vencimento do Titulo" as vencimento_titulo,
+        "Quantidade" as quantidade,
+        "Valor do Titulo" as valor_titulo,
+        "Valor da Operacao" as valor_operacao,
+        "Tipo da Operacao" as tipo_operacao,
+        "Canal da Operacao" as canal_operacao
+    FROM read_csv_auto('/workspaces/analytics_engineering_curse/curso/data/raw/operacoes.csv')
+),
+typed as (
+    select
+        codigo_investidor, 
+        data_operacao,
+        tipo_titulo,
+        vencimento_titulo,
+        quantidade,
+        valor_titulo,
+        valor_operacao,
+        tipo_operacao,
+        canal_operacao
+    from raw
+),
+
+final as (
+    select * from typed
+)
+select * from final
